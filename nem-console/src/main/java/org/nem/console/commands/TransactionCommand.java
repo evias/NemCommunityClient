a43e5821ff79644983697d2890348aaa9638ed9b
BloodyRookie <nemproject@gmx.de>
2016-10-23 15:32:19 +0200

    Remove TODO

--- nem-console/src/main/java/org/nem/console/commands/TransactionCommand.java
+++ nem-console/src/main/java/org/nem/console/commands/TransactionCommand.java
@@ -93,7 +93,6 @@ public abstract class TransactionCommand implements Command {
 	private static long feeFork(final int version) {
 		final byte network = (byte)(version >> 24);
 		return network == NetworkInfos.getMainNetworkInfo().getVersion()
-				// TODO: UPDATE BEFORE RELEASE
 				? 875_000
 				: (network == NetworkInfos.getMijinNetworkInfo().getVersion() ? 1 : 572_500);
 	}
